ecdc93f9e03cb806e5c429e3aa7b189415ff6c9e
Anouk <anouk138@gmail.com>
2024-10-15 21:05:49 -0600

    feat:frontend form integration

--- fronted/src/app/page.tsx
+++ fronted/src/app/page.tsx
@@ -1,13 +1,15 @@
+"use client";
+
+import VitalikPlace from './components/VitalikPlace';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <h1 className=" text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-        OLA K ASEN!
-        <br />
-        Diana, deje de abandonar
-       </h1>
+      <h1 className="text-2xl font-bold mb-8 font-[family-name:var(--font-geist-mono)]">
+          Vitalik Place
+        </h1>
+        <VitalikPlace />
       </main>
     </div>
   );
